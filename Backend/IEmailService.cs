26bd1c67ea7793835fca227336fb71944702d687
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-20 18:36:38 -0700

    IEmailService.cs

---
+++ Backend/IEmailService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using Sabio.Models.AppSettings;
+using Sabio.Models.Requests;
+using sib_api_v3_sdk.Model;
+
+namespace Sabio.Services
+{
+    public interface IEmailService
+    {
+        void SendTestEmail(SendEmailRequest request);
+        void SendAdminMessage(SendEmailRequest request);
+        void ContactUs(SendEmailRequest request);
+        void NewUserEmail(SendEmailRequest request, string confirmUrl);
+        void SendChangePasswordEmail(SendEmailRequest request, string token);
+        void UpdateUserPassword(SendEmailRequest request, string requestUrl);
+        void UpdateUserEmail(SendEmailRequest request, string requestUrl);
+    }
+}
