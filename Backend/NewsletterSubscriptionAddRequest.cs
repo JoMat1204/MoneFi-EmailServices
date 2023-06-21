0aa6dff3141812c35a2f62458cdc4a6fd593be80
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-20 18:34:33 -0700

    NewsletterSubscriptionAddRequest.cs

---
+++ Backend/NewsletterSubscriptionAddRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests
+{
+    public class NewsletterSubscriptionAddRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public bool IsSubscribed { get; set; }
+    }
+}                                                  
