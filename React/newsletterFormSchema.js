4496014d0f3a4a2a7dfe9880d01d1f94337a42b1
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-20 18:55:01 -0700

    newsletterFormSchema.js

---
+++ React/newsletterFormSchema.js
@@ -0,0 +1,6 @@
+import * as Yup from 'yup'
+const newsletterFormValidation = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+  });
+
+  export default newsletterFormValidation;
