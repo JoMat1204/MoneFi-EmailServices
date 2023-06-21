6720c48d0654c20ba8d5385a7d3d7280c76c105c
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-20 18:35:11 -0700

    NewsletterSubscription.cs

---
+++ Backend/NewsletterSubscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class NewsletterSubscription 
+    {
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(255)]
+        public string Email { get; set; }
+        public bool IsSubscribed { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
