b2bc6e5b506644d5fd1cb8fd8b6f0b5fa54570e5
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-20 18:32:45 -0700

    SendEmailRequest.cs

---
+++ Backend/SendEmailRequest.cs
@@ -0,0 +1,25 @@
+﻿using sib_api_v3_sdk.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests
+{
+    public class SendEmailRequest
+    {
+      [Required]
+      public EmailInfo Sender { get; set; }
+      [Required]
+      public EmailInfo To { get; set; }
+      [Required]
+      public string Subject { get; set; }
+      [Required]
+      public string Body { get; set; }
+
+
+    }
+}
