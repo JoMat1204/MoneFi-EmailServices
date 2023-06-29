7dcbeb1a713b86d5a2ef58bc9bca4c11a778fca6
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:47:10 -0700

    Update newsletterSubscriptionFormService.js

--- React/newsletterSubscriptionFormService.js
+++ React/newsletterSubscriptionFormService.js
@@ -4,70 +4,70 @@ import * as helper from "./serviceHelpers";
 const endpoint = `${helper.API_HOST_PREFIX}/api/newsletters/subscriptions`;
 
 const addEmailToSubscription = (email) => {
-  const config = {
-    method: "POST",
-    url: `${endpoint}`,
-    data: { email, isSubscribed: true },
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" },
-  };
-  return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
+const config = {
+  method: "POST",
+  url: `${endpoint}`,
+  data: { email, isSubscribed: true },
+  crossdomain: true,
+  headers: { "Content-Type": "application/json" },
+};
+return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
 };
 
 const listSubscribed = (pageIndex, pageSize) => {
-  const config = {
-    method: "GET",
-    url: `${endpoint}/paginate?pageIndex=${pageIndex}&pageSize=${pageSize}`,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" },
-  };
-  return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
+const config = {
+  method: "GET",
+  url: `${endpoint}/paginate?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+  crossdomain: true,
+  headers: { "Content-Type": "application/json" },
+};
+return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
 };
 
 const listAll = (pageIndex, pageSize) => {
-  const config = {
-    method: "GET",
-    url: `${endpoint}/paginate?pageIndex=${pageIndex}&pageSize=${pageSize}&all=false`,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" },
-  };
-  return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
+const config = {
+  method: "GET",
+  url: `${endpoint}/paginate?pageIndex=${pageIndex}&pageSize=${pageSize}&all=false`,
+  crossdomain: true,
+  headers: { "Content-Type": "application/json" },
+};
+return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
 };
 
 const searchPagination = (pageIndex, pageSize, query) => {
-  const config = {
-    method: "GET",
-    url: `${endpoint}/search?pageIndex=${pageIndex}&pageSize=${pageSize}&query=${query}&all=true`,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" },
-  };
-  return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
+const config = {
+  method: "GET",
+  url: `${endpoint}/search?pageIndex=${pageIndex}&pageSize=${pageSize}&query=${query}&all=true`,
+  crossdomain: true,
+  headers: { "Content-Type": "application/json" },
+};
+return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
 };
 
 const updateEmail = (payload) => {
-  const config = {
-    method: "PUT",
-    url: `${endpoint}`,
-    data: payload,
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" },
-  };
-  return axios(config)
-    .then(() => {
-      return payload;
-    })
-    .catch(helper.onGlobalError);
+const config = {
+  method: "PUT",
+  url: `${endpoint}`,
+  data: payload,
+  crossdomain: true,
+  headers: { "Content-Type": "application/json" },
+};
+return axios(config)
+  .then(() => {
+    return payload;
+  })
+  .catch(helper.onGlobalError);
 };
 
 const unsubscribeEmail = (email) => {
-  const config = {
-    method: "PUT",
-    url: `${endpoint}`,
-    data: { email, isSubscribed: false },
-    crossdomain: true,
-    headers: { "Content-Type": "application/json" },
-  };
-  return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
+const config = {
+  method: "PUT",
+  url: `${endpoint}`,
+  data: { email, isSubscribed: false },
+  crossdomain: true,
+  headers: { "Content-Type": "application/json" },
+};
+return axios(config).then(helper.onGlobalSuccess).catch(helper.onGlobalError);
 };
 
 export { addEmailToSubscription, listSubscribed, listAll, searchPagination, updateEmail, unsubscribeEmail };
