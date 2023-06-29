7ad6608cde3fc502dc2bf2464ef2172a6762a9b4
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:47:26 -0700

    Update securedRoutes.jsx

--- React/securedRoutes.jsx
+++ React/securedRoutes.jsx
@@ -1,18 +1,18 @@
 import { lazy } from "react";
 const NewsletterSubscriptionTable = lazy(() => import("../components/newslettersubscriptionsform/NewsletterSubscriptionTable"));
 const newsletterRoutes = [
-  {
-    path: "/newsletter/admin",
-    name: "NewsletterSubscriptionTable",
-    exact: true,
-    element: NewsletterSubscriptionTable,
-    roles: ["Admin"],
-    isAnonymous: false,
-  },
+{
+  path: "/newsletter/admin",
+  name: "NewsletterSubscriptionTable",
+  exact: true,
+  element: NewsletterSubscriptionTable,
+  roles: ["Admin"],
+  isAnonymous: false,
+},
 ];
 
 const allRoutes = [
-  ...newsletterRoutes,
+...newsletterRoutes,
 ];
 
 export default allRoutes;
