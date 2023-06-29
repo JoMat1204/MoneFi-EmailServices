430595d793edd95829f7d4e7d3fd789848bb9fcc
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:42:33 -0700

    Update EmailInfo.cs

--- Backend/EmailInfo.cs
+++ Backend/EmailInfo.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests
+public class EmailInfo 
 {
-    public class EmailInfo 
-    {
-        public string Email { get; set; }
-        public string Name { get; set; }
-    }
+    public string Email { get; set; }
+    public string Name { get; set; }
 }
