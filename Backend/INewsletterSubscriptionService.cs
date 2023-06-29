a76c28f698d40d68d945c2b4afb6eef20ec21c3f
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:43:34 -0700

    Update INewsletterSubscriptionService.cs

--- Backend/INewsletterSubscriptionService.cs
+++ Backend/INewsletterSubscriptionService.cs
@@ -1,17 +1,9 @@
-﻿using Sabio.Models;
-using Sabio.Models.Domain;
-using Sabio.Models.Requests;
-using System.Collections.Generic;
-
-namespace Sabio.Services
+public interface INewsletterSubscriptionService
 {
-    public interface INewsletterSubscriptionService
-    {
-        void Add(NewsletterSubscriptionAddRequest model);
-        Paged<NewsletterSubscription> GetAllPaginated(int pageIndex, int pageSize, bool? all = null);
-        Paged<NewsletterSubscription> SearchPaginated(int pageIndex, int pageSize, bool all, string query);
-        List<NewsletterSubscription> GetAllSubscribed();
-        NewsletterSubscription GetByEmail(string Email);
-        void Update(NewsletterSubscriptionAddRequest model);
-    }
+    void Add(NewsletterSubscriptionAddRequest model);
+    Paged<NewsletterSubscription> GetAllPaginated(int pageIndex, int pageSize, bool? all = null);
+    Paged<NewsletterSubscription> SearchPaginated(int pageIndex, int pageSize, bool all, string query);
+    List<NewsletterSubscription> GetAllSubscribed();
+    NewsletterSubscription GetByEmail(string Email);
+    void Update(NewsletterSubscriptionAddRequest model);
 }
