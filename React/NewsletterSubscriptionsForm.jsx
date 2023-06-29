8d5523a10055772d968f3acc8604d7b1f8a1221f
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:46:44 -0700

    Update NewsletterSubscriptionsForm.jsx

--- React/NewsletterSubscriptionsForm.jsx
+++ React/NewsletterSubscriptionsForm.jsx
@@ -8,79 +8,79 @@ import { Button } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 
 function NewsletterSubscriptions() {
-  const handleSubmit = (values, { setSubmitting , resetForm}) => {
-    newsletterSubscriptionFormService.addEmailToSubscription(values.email)
-      .then(onEmailSuccess)
-      .catch(onEmailFail)
-      .finally(() => {
-      setSubmitting(false);
-      resetForm();
-      })
-    };
-    const navigate = useNavigate();
-    
-  const onEmailSuccess = () =>{
-    Swal.fire("Thank You For Applying!", "Email added successfully!").then(() => {
-      navigate("/")
-      })
-    console.log("Email added successfully!");
-  }
+const handleSubmit = (values, { setSubmitting , resetForm}) => {
+  newsletterSubscriptionFormService.addEmailToSubscription(values.email)
+    .then(onEmailSuccess)
+    .catch(onEmailFail)
+    .finally(() => {
+    setSubmitting(false);
+    resetForm();
+    })
+  };
+  const navigate = useNavigate();
+  
+const onEmailSuccess = () =>{
+  Swal.fire("Thank You For Applying!", "Email added successfully!").then(() => {
+    navigate("/")
+    })
+  console.log("Email added successfully!");
+}
 
-  const onEmailFail = () => {
-    Swal.fire("Thank You!", "Your Email is already subscribed!")
-    console.log("Error adding email:");
-  }
+const onEmailFail = () => {
+  Swal.fire("Thank You!", "Your Email is already subscribed!")
+  console.log("Error adding email:");
+}
 
-  return (
-    <div className="newsletter-subscriptions">
-      <div className="newslettersubscriptions-container">
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="content">
-              <h2 className="newsletter-header">Newsletter Subscription</h2>
-              <Formik
-                initialValues={{ email: '' }}
-                validationSchema={newsletterFormValidation}
-                onSubmit={handleSubmit}
-                name="newsletterForm"
-              >
-                {({ errors, touched }) => (
-                  <Form>
-                    <div className="form-group">
-                      <h6 className="newsletter-email-text">Enter your email</h6>
-                      <div className="newsletter-input-group">
-                        <Field
-                          type="email"
-                          name="email"
-                          className={`newsletter-form-control ${
-                            errors.email && touched.email ? 'newsletter-is-invalid' : ''
-                          }`}
-                          placeholder="Enter email to subscribe"
-                        />
-                        <Button className="newsletter-btn-success" type="submit">
-                          Subscribe
-                        </Button>
-                      </div>
-                      <ErrorMessage
+return (
+  <div className="newsletter-subscriptions">
+    <div className="newslettersubscriptions-container">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="content">
+            <h2 className="newsletter-header">Newsletter Subscription</h2>
+            <Formik
+              initialValues={{ email: '' }}
+              validationSchema={newsletterFormValidation}
+              onSubmit={handleSubmit}
+              name="newsletterForm"
+            >
+              {({ errors, touched }) => (
+                <Form>
+                  <div className="form-group">
+                    <h6 className="newsletter-email-text">Enter your email</h6>
+                    <div className="newsletter-input-group">
+                      <Field
+                        type="email"
                         name="email"
-                        component="div"
-                        className="error text-danger"
+                        className={`newsletter-form-control ${
+                          errors.email && touched.email ? 'newsletter-is-invalid' : ''
+                        }`}
+                        placeholder="Enter email to subscribe"
                       />
+                      <Button className="newsletter-btn-success" type="submit">
+                        Subscribe
+                      </Button>
                     </div>
-                  </Form>
-                )}
-              </Formik>
-              <div className="text-center">
-                <a className="newsletter-link" href="">
-                  Want the latest news? Get it in your inbox.
-                </a>
-              </div>
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="error text-danger"
+                    />
+                  </div>
+                </Form>
+              )}
+            </Formik>
+            <div className="text-center">
+              <a className="newsletter-link" href="">
+                Want the latest news? Get it in your inbox.
+              </a>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default NewsletterSubscriptions;
