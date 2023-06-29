5f9c5cea7aa3114ba7feb091221d7b38c35360a9
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:36:26 -0700

    Update NewsletterSubscription_SelectByUserEmail.sql

--- SQL/NewsletterSubscription_SelectByUserEmail.sql
+++ SQL/NewsletterSubscription_SelectByUserEmail.sql
@@ -2,12 +2,10 @@ ALTER PROC [dbo].[NewsLetterSubscriptions_Select_ByEmail]
 @Email nvarchar(50)
 
 AS
-
 /*
 Declare @Email nvarchar(50) = 'testemail1@gmail.com'
 Execute dbo.NewsLetterSubscriptions_Select_ByEmail @Email 
 */
-
 BEGIN
 
 SELECT [Email]
