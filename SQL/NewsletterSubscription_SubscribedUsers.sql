92175f476ebb7886969068621a0a149cf02f9201
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:37:18 -0700

    Update NewsletterSubscription_SubscribedUsers.sql

--- SQL/NewsletterSubscription_SubscribedUsers.sql
+++ SQL/NewsletterSubscription_SubscribedUsers.sql
@@ -1,10 +1,3 @@
-﻿USE [MoneFi]
-GO
-/****** Object:  StoredProcedure [dbo].[NewsLetterSubscriptions_SelectAll_Subscribed]    Script Date: 6/20/2023 4:58:20 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 ALTER PROC [dbo].[NewsLetterSubscriptions_SelectAll_Subscribed]
 
 AS
