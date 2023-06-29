e507a338423fd12610e73e01b144fca121f0d7fe
JoMat1204 <122498046+JoMat1204@users.noreply.github.com>
2023-06-29 12:47:18 -0700

    Update publicRoutes.jsx

--- React/publicRoutes.jsx
+++ React/publicRoutes.jsx
@@ -1,15 +1,15 @@
 import { lazy } from "react";
 const NewsletterSubscriptionsForm = lazy(() => import("../components/newslettersubscriptionsform/NewsletterSubscriptionsForm"));
 const newsletterRoutes = [
-  {
-    path: "/newsletter/create",
-    name: "Newsletter Form",
-    exact: true,
-    element: NewsletterSubscriptionsForm,
-    roles: [],
-    isAnonymous: true,
-  },
-  var allRoutes = [
-  ...newsletterRoutes,
+{
+  path: "/newsletter/create",
+  name: "Newsletter Form",
+  exact: true,
+  element: NewsletterSubscriptionsForm,
+  roles: [],
+  isAnonymous: true,
+},
+var allRoutes = [
+...newsletterRoutes,
 ];
 export default allRoutes;
